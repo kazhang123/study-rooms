c3c98a9a74cb27a130ec176f4ae74266a940c39b
Katharine Zhang <katharine177@outlook.com>
2022-01-15 12:20:41 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+declare var ZoomMtg;
+
+ZoomMtg.setZoomJSLib("https://source.zoom.us/2.1.1/lib", "/av");
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load("en-US");
+ZoomMtg.i18n.reload("en-US");
 
 function App() {
+  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+  var signatureEndpoint = "http://localhost:4000";
+  var apiKey = "ylFH2WzbRXGkySuFN9Cnqg";
+  var meetingNumber = "76982752717";
+  var role = 1;
+  var leaveUrl = "http://localhost:3000";
+  var userName = "React";
+  var userEmail = "";
+  var passWord = "2WkeNi";
+  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
+  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
+  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
+  var registrantToken = "";
+
+  function getSignature(e) {
+    e.preventDefault();
+
+    fetch(signatureEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        startMeeting(response.signature);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function startMeeting(signature) {
+    document.getElementById("zmmtg-root").style.display = "block";
+
+    ZoomMtg.init({
+      leaveUrl: leaveUrl,
+      success: (success) => {
+        console.log(success);
+
+        ZoomMtg.join({
+          signature: signature,
+          meetingNumber: meetingNumber,
+          userName: userName,
+          apiKey: apiKey,
+          userEmail: userEmail,
+          passWord: passWord,
+          tk: registrantToken,
+          success: (success) => {
+            console.log(success);
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Zoom Meeting SDK Sample React</h1>
+
+        <button onClick={getSignature}>Join Meeting</button>
+      </main>
     </div>
   );
 }
